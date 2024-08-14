21229032ed7969d8a5a3b49e383dfbc15176852d
david820428 <david820428@yahoo.com.tw>
2024-08-14 11:30:55 +1000

    change the price source to "a", "b"

--- pairArbitrage.js
+++ pairArbitrage.js
@@ -13,7 +13,8 @@ window.onload = function() {
             const stockSymbols = ["1229.tw", "2355.tw", "1605.tw", "3059.tw", "6202.tw", "2376.tw", "2034.tw", "2002.tw", "1210.tw", "1307.tw", "2441.tw", "2387.tw", "2103.tw", "3532.tw", "8341.tw", "6770.tw", "2421.tw", "2498.tw", "9930.tw"];
             
             stockSymbols.forEach((symbol, index) => {
-                const stockPrice = data.msgArray.find(stock => stock.ch === symbol)?.pz || "N/A";
+                const stockPrice_raw = data.msgArray.find(stock => stock.ch === symbol)?
+                const stockPrice = (stockPrice_raw.a.split('_')[0] + stockPrice_raw.b.split('_')[0])/2 || "N/A";
                 console.log(`Extracted stock price for ${symbol}:`, stockPrice);
                 document.getElementById(`stockPrice${index + 1}`).textContent = stockPrice;
             });
